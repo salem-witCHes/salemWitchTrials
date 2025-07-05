996bed0d13cd109f23c8bfba3bd36c5776ef7b58
Miriana <mirianaa.pinto@gmail.com>
2025-07-05 03:08:28 +0200

    update

--- csvToRDF.py
+++ csvToRDF.py
@@ -17,9 +17,14 @@ FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 namespaces = {
     "crm": Namespace("http://www.cidoc-crm.org/cidoc-crm/"),
     "schema": Namespace("https://schema.org/"),
-    "dcterms": Namespace("(http://purl.org/dc/terms/"),
+    "dcterms": Namespace("http://purl.org/dc/terms/"),
     "fabio": Namespace("http://purl.org/spar/fabio"),
-    "foaf": Namespace("http://xmlns.com/foaf/0.1/")
+    "foaf": Namespace("http://xmlns.com/foaf/0.1/"),
+    "wd": Namespace("https://www.wikidata.org/wiki/"),
+    "skos": Namespace("http://www.w3.org/2004/02/skos/core#"),
+    "owl": Namespace("http://www.w3.org/2002/07/owl#"),
+    "rdf": Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+    "viaf": Namespace("http://viaf.org/viaf/")
 }
 
 # Predicate mapping
@@ -36,8 +41,12 @@ with open("mapping.csv", encoding="utf-8") as f:
 
 # Entities mapping 
 mapping_entities = {}
-with open(path, encoding="utf-8") as f:
+with open("mapping_entities.csv", encoding="utf-8") as f:
     reader = csv.DictReader(f)
+    for row in reader:
+        mapping_entities[row["object"]] = row["entity"]
+
+# print(my_entities)
 
 
 # Initialize RDF graph
@@ -61,38 +70,38 @@ for file in os.listdir(folder):
 
                 csv_items.append(subj)
 
-            # Replace natural language predicate with property
-            if pred in predicate_map:
-                predicate = predicate_map[pred]
-            else:
-                print(f"Unknown predicate '{pred}'") # If predicate match is missing
-                predicate = RDFS.comment
-
-            # Check if object is entity or literal
-            if obj in csv_items:
-                # create URIRef from base for it
-                object = URIRef(BASE + csv_items_prefix + obj)
-            elif obj in mapping_entities:
-                entity = mapping_entities[row["obj"]]
-                if ":" not in entity:
-                    # Simply use the base URI to create the object
-                    obj = URIRef(BASE + entity)
+                # Replace natural language predicate with property
+                if pred in predicate_map:
+                    predicate = predicate_map[pred]
                 else:
-                    # Else split namespace and entity
-                    namespace_str, entity_str = entity.split(":")
-                    namespace = namespaces[namespace_str]
-                    # Create object URI
-                    obj = URIRef(namespace + entity_str)
-            else:
-                # Otherwise create a Literal object
-                obj = Literal(obj)
+                    print(f"Unknown predicate '{pred}'") # If predicate match is missing
+                    predicate = RDFS.comment
 
-            # Add triple to the graph
-            g.add((subj, predicate, obj_lit))
+                # Check if object is entity or literal
+                if obj in csv_items:
+                    # create URIRef from base for it
+                    object = URIRef(BASE + csv_items_prefix + obj)
+                elif obj in mapping_entities:
+                    entity = mapping_entities[obj]
+                    if ":" not in entity:
+                        # Simply use the base URI to create the object
+                        obj = URIRef(BASE[entity])
+                    else:
+                        # Else split namespace and entity
+                        namespace_str, entity_str = entity.split(":")
+                        namespace = namespaces[namespace_str]
+                        # Create object URI
+                        obj = URIRef(namespace + entity_str)
+                else:
+                    # Otherwise create a Literal object
+                    obj = Literal(obj)
 
-for s,p,o in g.triples((None, None, None)):
-    print(s,p,o)
+                # Add triple to the graph
+                g.add((subj, predicate, obj))
+
+# for s,p,o in g.triples((None, None, None)):
+#     print(s,p,o)
 
 # Serialize the graph to Turtle format
-g.serialize(destination="first_try.ttl", format="ttl")
+g.serialize(destination="second_try.ttl", format="ttl")
 
